833408399d6d837138ca5af87ca42d10708f8a4c
Aécio Brumel <aeciobrumelms@gmail.com>
2026-03-12 20:04:03 -0300

    feat(front): bootstrap DevStore frontend with base layout and static assets
    - initialize Next.js frontend structure with Tailwind CSS support
    - add root and site layouts with global styles and metadata
    - create header component with promotional shipping banner
    - add homepage entrypoint and public assets for banners, products, and UI icons

---
+++ front/src/app/(site)/layout.tsx
@@ -0,0 +1,15 @@
+import { Header } from "@/components/layout/header";
+
+export default function SiteLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div>
+      <Header />
+      {children}
+    </div>
+  );
+}
+
---
+++ front/src/components/layout/header.tsx
@@ -0,0 +1,9 @@
+export function Header() {
+    return (
+        <header >
+            <div className="bg-black text-white text-[16px] p-4 text-center">
+                <strong>FRETE GRÁTIS</strong> para todo o Nordeste nas compras acima de R$ 199,00.<strong> APROVEITA!</strong>
+            </div>
+        </header>
+    );
+}
