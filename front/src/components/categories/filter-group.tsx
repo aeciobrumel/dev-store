7968ce8110ee90da6bf6ff0ae80c664c0474838d
Aécio Brumel <aeciobrumelms@gmail.com>
2026-06-07 17:07:26 -0300

    feat(categories): adiciona colapso expansível ao grupo de filtros
    - adiciona controle local de abertura/fechamento com `useState`
    - substitui placeholder por ícone com rotação animada
    - aplica transição na exibição dos itens do grupo
    - ajusta espaçamento vertical dos itens de filtro

--- front/src/components/categories/filter-group.tsx
+++ front/src/components/categories/filter-group.tsx
@@ -1,15 +1,24 @@
+import Image from "next/image";
 import { FilterItem } from "./filter-item";
+import { useState } from "react";
 
 export const FilterGroup = () => {
+    const [opened, setOpened] = useState(true);
     return (
         <div className="mb-8">
-            <div className="flex justify-between items-center border-b border-gray-200 pb-4">
+            <div className="flex justify-between items-center border-b border-gray-200 pb-4 mb-8">
                 <div className="flex-1 font-bold text-xl">Nome do grupo</div>
-                <div className="">
-                    ...
+                <div className="size-8 flex justfy-center-items-center cursor-pointer" onClick={() => setOpened(!opened)}>
+                    <Image
+                        className={`${opened ? 'rotate-0' : 'rotate-180'} transition-transform duration-300`}
+                        src={'/assets/ui/arrow-left-s-line.png'}
+                        alt="arrow-left"
+                        width={24}
+                        height={24}
+                    />
                 </div>
             </div>
-            <div className="pt-8 flex flex-col gap-4">
+            <div className={`flex flex-col overflow-y-hidden ${opened ? 'max-h-96' : 'max-h-0'} transition-all duration-300`}>
                 <FilterItem id={1} label={'item1'} />
                 <FilterItem id={2} label={'item2'} />
                 <FilterItem id={3} label={'item3'} />
--- front/src/components/categories/filter-item.tsx
+++ front/src/components/categories/filter-item.tsx
@@ -5,7 +5,7 @@ type Props = {
 
 export const FilterItem = ({ id, label }: Props) => {
     return (
-        <div className="flex gap-4 items-center text-gray-500 ">
+        <div className="flex gap-4 items-center text-gray-500 mt-4 ">
             <input type="checkbox" className="size-6" id={`ck-${id}`} />
             <label htmlFor={`ck-${id}`} className="ml-2" >{label}</label>
         </div>
